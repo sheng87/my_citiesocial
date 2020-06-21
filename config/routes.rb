8152f5130051fa0774d248823cda3dd29b3de84e
sheng87 <4004171022kyu@gmail.com>
2020-06-21 18:19:12 +0800

    sign_up and log_in path set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  devise_for :users
+  root 'products#index'
+
+  resources :products, only: [:index, :show]
 end
