8af49e830eb7a3cdb948d036842fb0b2f88424cf
sheng87 <4004171022kyu@gmail.com>
2020-06-25 00:31:10 +0800

    google plugin

--- Gemfile
+++ Gemfile
@@ -26,6 +26,8 @@ gem 'jbuilder', '~> 2.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 gem 'devise', '~> 4.2'
+gem 'omniauth-google-oauth2', '~> 0.4.1'
+gem "figaro"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/frontend/scripts/shared/application.js
+++ app/frontend/scripts/shared/application.js
@@ -12,3 +12,13 @@ document.addEventListener('turbolinks:load', () => {
       });
     }
   });
+
+  document.addEventListener('DOMContentLoaded', () => {
+    (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+      $notification = $delete.parentNode;
+  
+      $delete.addEventListener('click', () => {
+        $notification.parentNode.removeChild($notification);
+      });
+    });
+  });
